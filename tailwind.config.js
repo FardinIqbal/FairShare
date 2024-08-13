a421e7e0ddd1efc503f13a1b1e12970c963ba014
LemonTea333 <52869603+FardinIqbal@users.noreply.github.com>
2024-08-12 22:05:17 -0400

    Update .gitignore to exclude unnecessary files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css', // Add this line if you want to include CSS files
+    './app/assets/stylesheets/**/*.scss' // Include SCSS files if you are using them
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
