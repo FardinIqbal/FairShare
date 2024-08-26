b4047c0f548c04e3413654b2dcc6af1a8164fa6d
LemonTea333 <52869603+FardinIqbal@users.noreply.github.com>
2024-08-25 23:07:03 -0400

    add user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :destroy]
 
+  has_many :expenses, dependent: :destroy
+  has_many :balances, dependent: :destroy
   def show
-    @user = User.find(params[:id])
     @groups = @user.groups
     @total_balance = @user.total_balance
     @recent_expenses = @user.expenses.order(created_at: :desc).limit(5)
     @expense_categories = @user.expenses.group(:category).sum(:amount)
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to root_path, notice: 'User was successfully deleted.'
+    else
+      redirect_to user_path(@user), alert: 'Failed to delete user. Please try again.'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
